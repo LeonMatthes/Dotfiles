4b339e4103445bc62034765a5d7ba339db1808ed
Leon Matthes <leon@matthes.biz>
2019-07-03 13:51:52 +0200

    Update dotfiles

--- .config/i3/lock-screen.sh
+++ .config/i3/lock-screen.sh
@@ -6,6 +6,6 @@ tmpbg='/tmp/screen.png'
 (( $# )) && { icon=$1; }
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 20% -blur 0x3 -scale 500% "$tmpbg"
+convert "$tmpbg" -scale 20% -blur 0x2  -resize 500% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -n -i "$tmpbg" --pointer=default -f
+i3lock --nofork -i "$tmpbg" --pointer=default --show-failed-attempts
