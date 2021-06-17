0941989aa1ea8e8a243173f0185562af452d7e70
Leon Matthes <leon@matthes.biz>
2021-06-17 20:36:34 +0200

    Improve initial install script

--- .config/dotfiles/install.fish
+++ .config/dotfiles/install.fish
@@ -1,4 +1,28 @@
 #!/usr/bin/env fish
 
+function inform
+  set_color green
+  printf "\n###### $argv #########\n"
+  set_color normal
+end
+
+inform Installing Oh my fish
+curl -L https://get.oh-my.fish | fish
+
+omf install fish_logo
+
+inform Installing Vim-Plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+inform Installing pynvim
+sudo pip install pynvim
+
+inform Changing default shell to fish
+chsh -s (which fish)
+
+inform Installing powerline fonts
+~/Documents/software/powerline-fonts/install.sh
+
+inform Configuration done - log out and back in
+
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Installation
 
-Run in fish:
+Run in fish shell:
 ``` shell
 git clone --bare git@github.com:LeonMatthes/Dotfiles.git $HOME/.dotfiles
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+dotfiles submodule update --init --recursive
 dotfiles checkout
+dotfiles config status.showUntrackedFiles no
+$HOME/.config/dotfiles/install.fish
 ```
